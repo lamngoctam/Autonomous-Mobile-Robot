5374021bc1088e7de83aa19db817f0312357fc8a
lamngoctam <lamngoctam@gmail.com>
2021-01-01 08:32:38 +0800

    run catkin_make

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/tam/Documents/github-Projects/Autonomous-Mobile-Robot/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/tam/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/tam/catkin_ws/devel;/opt/ros/melodic' != '' else []
